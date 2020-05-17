48d3bde48329e7fb22036540384fc30be313edc5
3v4ld0 <evaldocomp@gmail.com>
2020-05-17 01:21:02 -0300

    Update dir_help.py

--- Lendo_Recebendo_dados/dir_help.py
+++ Lendo_Recebendo_dados/dir_help.py
@@ -1,14 +1,17 @@
 """
-Utilitário python para te auxiliar na programação
-Dir - > Apresenta todos os atributos e funções /metodos disponiveis para
-deterniando tipo de dado ou variavel
+O Dir e Help são formas de se obter informações do próprio Pythom em tempo real, 
+com alguns comandos você consegue uma lista de dicas possibilidades que a linguagem oferece.
 
-
-Help - > Aprtensta a documewntação/comoutilizar os atributos/propriedades e
- funções / metodos disponiveis
-para determniado tipo de dado ou varivel"
-help('tipo de dado'.lower)
-
-
-
+Dir - > Lista os atributos e funções que um tipo de dado pode suportar em Python
 """
+#print(dir ('eu'))
+
+
+
+#Help - > Mostra  a documentação/como utilizar os atributos/propriedades e
+ #funções / metodos disponiveis para determniado tipo de dado ou varivel
+
+
+#help('tipo de dado'.lower) #Mostra o que  o .lower faz em uma string
+
+print("Digite seu nome".lower())
