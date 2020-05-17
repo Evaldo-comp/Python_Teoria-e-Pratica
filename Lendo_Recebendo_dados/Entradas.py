fca1e672f00310ff78d62ec3766596f433185c92
3v4ld0 <evaldocomp@gmail.com>
2020-05-17 03:38:36 -0300

    Entradas

---
+++ Lendo_Recebendo_dados/Entradas.py
@@ -0,0 +1,28 @@
+# Recebendo dados do usuário
+
+# Entrada de dados
+print("Qual o seu nome?")
+nome = input()
+print("Seja bem vindo ", nome)
+
+# Entrada de dados utlizando case
+print("Qual a sua idade?")
+idade = (input())
+print("você tem", str(idade), " anos")
+
+# Entrada de dados utlizando case
+print("Qual a sua idade?")
+idade = int(input())
+print("Você tem ", idade, "anos")
+
+# Entrada de Dados em linha única
+# inteiros
+idade = int(input("Qual sua idade?"))
+
+# Strings
+nome = input("Qual seu Nome?")
+
+# Floats
+altura = float(input("Qual sua Altura?"))
+
+print(idade, nome, altura)
