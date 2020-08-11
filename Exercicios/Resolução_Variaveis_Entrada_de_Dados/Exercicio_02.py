d37419cf22500e960d8aafc7c225c4c0e6813722
Francisco Evaldo <65002701+Evaldo-comp@users.noreply.github.com>
2020-08-11 14:28:17 -0300

    Update and rename Exercicio_03-06.py to Exercicio_02.py

--- Exercicios/Resolução_Variaveis_Entrada_de_Dados/Exercicio_02.py
+++ Exercicios/Resolução_Variaveis_Entrada_de_Dados/Exercicio_02.py
@@ -7,9 +7,11 @@ armazenada nas seguintes variáveis:materia1, materia2, materia3
 materia1 = 4.7
 materia2 = 5.8
 materia3 = 1.5
-# Solução
+
 media1 = (5.2 + 7.8 + 3.5 + materia1) / 4
 media2 = (7.2 + 8.8 + 9.5 + materia2) / 4
 media3 = (1.2 + 4.8 + 6.5 + materia3) / 4
 
 print((media1 and media2 and media3) > 7)
+
+#Fonte: Introdução à Programação com Python - Nilo Ney Coutinho- 3º ed
