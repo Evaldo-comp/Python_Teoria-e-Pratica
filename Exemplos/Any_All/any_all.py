ef7d0088ae879154870a87327d4d63c99a95c6c0
Evaldo-comp <evaldocomp@gmail.com>
2020-11-03 15:50:26 -0300

    teste

--- Exemplos/Any_All/any_all.py
+++ Exemplos/Any_All/any_all.py
@@ -3,14 +3,14 @@
 all() - retorna True se todos os iteráveis são verdadeiros ou ainda se o conjunto de interáveis estiver vazio
 any () - retorna True se algum elemento do iterável for verdadeiro, se o iterável estiver vazio, retorna False
 """
-#Exemplo all()
+# Exemplo all()
 
-print(all([0,1,2,3,4,5]))
+print(all([0, 1, 2, 3, 4, 5]))
 
 # Exemplo 2
 
-print(all([num for num in [4, 2, 10, 8] if num %2 ==0]))
+print(all([num for num in [4, 2, 10, 8] if num % 2 == 0]))
 
 # Exemplo any()
 
-print(any([1,2,3,4,5,6]))
+print(any([1, 2, 3, 4, 5, 6]))
