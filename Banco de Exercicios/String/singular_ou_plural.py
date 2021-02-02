15b1d0d7e5a9bf7437fc1e8fede9cd60a9331f95
Evaldo-comp <evaldocomp@gmail.com>
2021-02-02 17:05:18 -0300

    organizacao_pastas

--- Banco de Exercicios/String/singular_ou_plural.py
+++
@@ -1,7 +0,0 @@
-"""
-Crie uma função que determine quando uma palavra está no plural, retornando True ou False
-"""
-def is_plural(word):
-    return word.endswith("s")
-
-print(is_plural("Evaldos"))
