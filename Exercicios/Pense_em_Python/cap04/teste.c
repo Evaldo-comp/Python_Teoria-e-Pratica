8d9872e98eddd4642e9d74eb49d3da370a63db0e
Evaldo-comp <evaldocomp@gmail.com>
2021-02-02 03:01:20 -0300

    update img

---
+++ Exercicios/Pense_em_Python/cap04/teste.c
@@ -0,0 +1,21 @@
+int main(){
+
+int calculalista (Lista* l) {
+
+    int i =0;
+
+    Lista* aux= Lista malloc (Sizeof(Lista));
+
+    aux= l
+
+    while(aux != NULL){
+
+        i++;
+
+        aux= aux->prox;  //(ponteiro aux recebe a próxima posição da lista)
+
+    }
+
+    return i;
+
+}}
